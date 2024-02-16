492c58ffca19aa31c4808737527ba14d6543f5a0
lovemxchine <lovemxchine@gmail.com>
2024-02-16 23:47:12 +0700

    third test

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'hello yoyo'),
+      home: const MyHomePage(title: 'hello 1223yoyo'),
     );
   }
 }
